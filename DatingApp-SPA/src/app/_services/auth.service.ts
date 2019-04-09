9d550ae7afd35f43284d57454903b3dd225e633e
aymanmanee3 <aymanmanee3@gmail.com>
2019-04-09 14:02:02 +0200

    Section6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
   constructor(private http: HttpClient) {
 
   }
@@ -15,12 +18,16 @@ export class AuthService {
       map((response: any) => {
         const user = response;
         if (user) {
-          localStorage.setItem('token', user);
+          localStorage.setItem('token', user.token);
         }
       })
     );
   }
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -22,12 +23,11 @@ export class RegisterComponent implements OnInit {
 
   register() {
     this.auth.register(this.model).subscribe(success => {
-      console.log(success);
+      this.alertify.success('Registeration successful');
     },
       error => {
-        console.log(error);
+        this.alertify.error(error);
       });
-    console.log(this.model);
   }
 
 }
